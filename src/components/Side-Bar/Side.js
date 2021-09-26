6d92b3c56ff3ca267d2405c7988e1a0ac5acb499
Arvind <arbindudebnath59985@gmail.com>
2021-09-26 23:36:06 +0600

    all complete

--- src/components/Side-Bar/Side.js
+++ src/components/Side-Bar/Side.js
@@ -4,15 +4,24 @@ const Side = (props) => {
     const {cart} = props
     console.log(cart)
     const total = cart?.reduce((previous, product) => previous + parseInt(product?.price), 0)
+    
+    const sideStyle ={
+    border: '3px solid indianred',
+    padding:'20px 30px', 
+    borderRadius:'10px', 
+    position: 'fixed'
+}
+
 
     return (
-        <div style={{border: '3px solid indianred', padding:'20px 30px', borderRadius:'10px', position: 'fixed'}}>
-            <h5>Food Added: {cart.length}</h5>
-            <h6>Total: {total}</h6>
-            <hr />
-            <h6>Added Meal</h6>
+        <div style={sideStyle}>
+            <h4>Added Meal</h4>
             <ul>{cart.map((meal,i)=><li key={i}>{meal.strMeal}</li>
             )}</ul>
+            <hr />
+            
+            <h6>Food Added: {cart.length}</h6>
+            <small>Total: {total}</small>
         </div>
     );
 };
