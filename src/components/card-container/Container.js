9fe2b95377cf205d2bd3540caf3a3284c94e926f
Arvind <arbindudebnath59985@gmail.com>
2021-09-26 14:20:35 +0600

    ui commplete

--- src/components/card-container/Container.js
+++ src/components/card-container/Container.js
@@ -1,28 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import './Container.css'
 import Card from '../card/Card';
+import Side from '../Side-Bar/Side';
 
 const Container = () => {
     const [items, setItems] = useState([])
+    const [cart, setCart] = useState([])
 
     useEffect(() =>{
-        fetch('./food.Json')
+        fetch('./food.JSON')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setItems(data))
     },[])
+
+
+    const addToCart = (item) => {
+        const newCart = [...cart, item]
+        setCart(newCart)
+
+    }
+    
+
 
 
     return (
         <div className='main-container'>
             <div className='main'>
                 <h1>Hi there</h1>
-                <Card></Card>
+                {
+                    items.map(item => <Card item={item} addToCart = {addToCart}></Card>)
+                }
             </div>
             <div className='side'>
-                <h1>Hello there</h1>
+                {/* <h2>Added Food: {cart.length}</h2>
+                <h2>{console.log(cart)}</h2> */}
+                <Side cart = {cart}></Side>
             </div>
         </div>
     );
 };
+
+
 
 export default Container;
